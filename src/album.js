7309333f54def683c91ef84458cd299fffbdc39e
Vitor José de Souza Lopes Sacramento <vitor.sacramento@MIDGARD.LOCAL>
2019-08-21 18:25:14 -0300

    spw-01 - Ajustando módulo request para utilizar o singleton do sessionManager

--- src/album.js
+++ src/album.js
@@ -3,27 +3,15 @@ import request from './request';
 import sessionManager from './sessionManager';
 
 function album() {
-  const current_session = sessionManager.getInstance();
-  const custom_header = {
-    headers: {
-      'Content-Type': 'application/json',
-      Accept: 'application/json',
-      Authorization: ''
-    }
-  }
-
   return {
     getAlbum: (id) => {
-      custom_header.headers.Authorization = `Bearer ${current_session.oAuthState.access_token}`;
-      return request(`${API_URL}/album/${id}?market=ES`, custom_header)
+      return request(`${API_URL}/album/${id}?market=ES`)
     },
     getAlbums: (ids) => {
-      custom_header.headers.Authorization = `Bearer ${current_session.oAuthState.access_token}`;
-      return request(`${API_URL}/albums?ids=${ids}&market=ES`, custom_header)
+      return request(`${API_URL}/albums?ids=${ids}&market=ES`)
     },
     getTracks: (id) => {
-      custom_header.headers.Authorization = `Bearer ${current_session.oAuthState.access_token}`;
-      return request(`${API_URL}/album/${id}/tracks?market=ES`, custom_header)
+      return request(`${API_URL}/album/${id}/tracks?market=ES`)
     }
   }
 }
--- src/request.js
+++ src/request.js
@@ -1,5 +1,18 @@
-const request = (url, headers) => {
-  return fetch(url, headers)
+import sessionManager from './sessionManager';
+
+const request = (url, customHeader) => {
+  const curretSession = sessionManager.getInstance();
+  const defaultHeader = {
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+      Authorization: `Bearer ${curretSession.oAuthState.access_token}`
+    }
+  };
+
+  const header = customHeader ? customHeader : defaultHeader;
+  
+  return fetch(url, header)
     .then(data => data.json())
     .catch(err => console.log(`Could not retrive data from API: ${err}`));
 }
